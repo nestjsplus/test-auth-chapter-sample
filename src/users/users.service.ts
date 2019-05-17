a9bbff54fc8335cac02922afbf9f6f41656e47e3
John Biundo <johnfbiundo@gmail.com>
2019-05-17 08:13:41 -0700

    fix userId property of users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,16 +8,19 @@ export class UsersService {
   constructor() {
     this.users = [
       {
+        userId: 1,
         username: 'john',
         password: 'changeme',
         pet: { name: 'alfred', picId: 1 },
       },
       {
+        userId: 2,
         username: 'chris',
         password: 'secret',
         pet: { name: 'gopher', picId: 2 },
       },
       {
+        userId: 3,
         username: 'maria',
         password: 'guess',
         pet: { name: 'jenny', picId: 3 },
