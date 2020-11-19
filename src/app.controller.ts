0bd4491405146d56136e031610c86a1e1c96d835
LIM KWANG KYU <kk.lim@gsshop.com>
2020-11-19 20:34:04 +0900

    feat : swagger documents

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,37 +13,45 @@ import { Response } from 'express';
 import { LoginGuard } from './common/guards/login.guard';
 import { AuthenticatedGuard } from './common/guards/authenticated.guard';
 import { AuthExceptionFilter } from './common/filters/auth-exceptions.filter';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
+import { LoginDTO } from './login.dto';
 
 @Controller()
 @UseFilters(AuthExceptionFilter)
 export class AppController {
 
+  @ApiOperation({ summary: 'Login Page' })
   @Get('/')
   @Render('login')
-  index(@Request() req, @Res() res: Response): {message: string} {
+  index(@Request() req, @Res() res: Response): { message: string } {
     return { message: req.flash('loginError') };
   }
 
+  @ApiOperation({ summary: 'Login on Local Strategy' })
+  @ApiBody({ type: LoginDTO })
   @UseGuards(LoginGuard)
   @Post('/login')
   login(@Request() req, @Res() res: Response): void {
     res.redirect('/home');
   }
 
+  @ApiOperation({ summary: 'Home Page' })
   @UseGuards(AuthenticatedGuard)
   @Get('/home')
   @Render('home')
-  getHome(@Request() req, @Res() res: Response): {user: any } {
+  getHome(@Request() req, @Res() res: Response): { user: any } {
     return { user: req.user };
   }
 
+  @ApiOperation({ summary: 'Profile Page' })
   @UseGuards(AuthenticatedGuard)
   @Get('/profile')
   @Render('profile')
-  getProfile(@Request() req: any, @Res() res: Response): {user: any} {
+  getProfile(@Request() req: any, @Res() res: Response): { user: any } {
     return { user: req.user };
   }
 
+  @ApiOperation({ summary: 'Logout' })
   @Get('/logout')
   logout(@Request() req, @Res() res: Response): void {
     req.logout();
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import * as session from 'express-session';
 import flash = require('connect-flash');
 import * as exphbs from 'express-handlebars';
 import * as passport from 'passport';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -15,6 +16,14 @@ async function bootstrap() {
   app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }));
   app.set('views', viewsPath);
   app.set('view engine', '.hbs');
+
+  const options = new DocumentBuilder()
+    .setTitle('Workplace Feed Server')
+    .setDescription('워크플레이스의 특정 피드를 전달 받는 서비스')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
 
   app.use(
     session({
